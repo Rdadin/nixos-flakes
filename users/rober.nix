b512b8e8c9ea367a2310e5344e53c0b13758e149
Roberto Dadin <r.dadin94@gmail.com>
2025-08-07 01:29:07 -0300

    feat: alacrity config + starship prompt

--- users/rober.nix
+++ users/rober.nix
@@ -18,8 +18,14 @@
 
   programs.alacritty = {
     enable = true;
-
     settings = {
+      colors.primary = {
+        background = "#1a1b26"; 
+        foreground = "#c0caf5"; 
+        };
+      window = {
+        opacity = 0.9;
+      };
       font = {
         normal = {
           family = "FiraCode Nerd Font";
@@ -49,13 +55,76 @@
   programs.starship = {
     enable = true;
     settings = {
-        add_newline = false;
-        format = "$directory$git_branch$git_status$nix_shell$character ";
-        character.success_symbol = "[➜](green)";
-        character.error_symbol = "[✗](red)";
+      add_newline = false;
+      scan_timeout = 10;
+      format = lib.concatStrings [
+        "[░▒▓](#a3aed2)"
+        "[  ](bg:#a3aed2 fg:#090c0c)"
+        "[](bg:#769ff0 fg:#a3aed2)"
+        "$directory"
+        "[](fg:#769ff0 bg:#394260)"
+        "$git_branch"
+        "$git_status"
+        "[](fg:#394260 bg:#212736)"
+        "$nodejs"
+        "$rust"
+        "$golang"
+        "$php"
+        "[](fg:#212736 bg:#1d2230)"
+        "$time"
+        "[ ](fg:#1d2230)"
+        "\n$character"
+      ];
+      directory = {
+        style = "fg:#e3e5e5 bg:#769ff0";
+        format = "[ $path ]($style)";
+        truncation_length = 3;
+        truncation_symbol = "…/";
+        substitutions = {
+          "Documents" = "󰈙 ";
+          "Downloads" = " ";
+          "Music" = " ";
+          "Pictures" = " ";
+        };
+      };
+      git_branch = {
+        symbol = "";
+        style = "bg:#394260";
+        format = "[[ $symbol $branch ](fg:#769ff0 bg:#394260)]($style)";
+      };
+      git_status = {
+        style = "bg:#394260";
+        format = "[[($all_status$ahead_behind )](fg:#769ff0 bg:#394260)]($style)";
+      };
+      nodejs = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      rust = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      golang = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      php = {
+        symbol = "";
+        style = "bg:#212736";
+        format = "[[ $symbol ($version) ](fg:#769ff0 bg:#212736)]($style)";
+      };
+      time = {
+        disabled = false;
+        time_format = "%R";
+        style = "bg:#1d2230";
+        format = "[[  $time ](fg:#a0a9cb bg:#1d2230)]($style)";
+      };
     };
   };
-
+ 
   programs.bash = {
     enable = true;
     shellAliases = {
