5bcede7565dc89dd2042fa148ce4d6a86b1f5e0b
Roberto Dadin <r.dadin94@gmail.com>
2025-08-10 01:06:18 -0300

    fix: remove blueman

--- modules/services/bluetooth.nix
+++ modules/services/bluetooth.nix
@@ -5,6 +5,5 @@ with lib; {
   config = mkIf config.services.bluetooth.enable {
     hardware.bluetooth.enable = true;
     hardware.bluetooth.powerOnBoot = true;
-    services.blueman.enable = true;
   };
 }
