058990f9fa619baef151d6d6cd93c135f4773f24
Roberto Dadin <r.dadin94@gmail.com>
2025-08-11 03:23:19 -0300

    fix: replug usb

--- modules/hardware/usb-receivers.nix
+++ modules/hardware/usb-receivers.nix
@@ -1,6 +1,6 @@
 # Rebind Logitech receivers so they appear as if replugged when detected
 
-{ ... }:
+{ pkgs, ... }:
 {
 
   services.udev.extraRules = ''
@@ -12,5 +12,21 @@
     ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="046d", ATTR{idProduct}=="c545", \
       RUN+="/bin/sh -c 'echo -n $kernel > /sys/bus/usb/drivers/usbhid/unbind; sleep 0.5; echo -n $kernel > /sys/bus/usb/drivers/usbhid/bind'"
   '';
+
+  systemd.services."input-retrigger-after-dm" = {
+  description = "Retrigger input devices after display manager starts";
+  after = [ "display-manager.service" ];
+  wants = [ "display-manager.service" ];
+  serviceConfig = {
+    Type = "oneshot";
+    ExecStart = [
+      "${pkgs.udev}/bin/udevadm control --reload"
+      # retrigger only input subsystem; fast, no sleeps
+      "${pkgs.udev}/bin/udevadm trigger --subsystem-match=input --action=add"
+    ];
+  };
+  wantedBy = [ "multi-user.target" ];
+};
+
 }
 
